7b14598d2e3f15ad0bb994e1966616a2d8de2c09
xiaomingwnagtao <xm@sina.com>
2019-04-25 18:02:49 +0800

    初始化

---
+++ src/components/detail.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        详情页
+    </div>
+</template>
+
+
+<script>
+export default {
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+
+<style lang="less">
+
+
+
+</style>
+
+
+
+
+
+
+
+
---
+++ src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <span>
+            abc
+        </span>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+
+<style lang="less" scoped>
+
+div{
+   span{
+       color:yellow;
+   }
+}
+
+</style>
+
+
+
+
+
+
+
+
